b95a828c7dede7c63aa0aea2d4980cf281cb9494
slymnslhn <baverslhn@gmail.com>
2025-10-23 23:35:58 -0400

    locators

--- demo-test/tests/firstTest.spec.ts
+++ demo-test/tests/firstTest.spec.ts
@@ -3,14 +3,36 @@ import { test } from "@playwright/test";
 test.beforeEach(async ({ page }) => {
    await page.goto('http://localhost:4200/');
    await page.getByText('Forms').click();
+     await page.getByText('Form Layouts').click();
 });
 
 
-test('the first test', async ({page}) => {
-    await page.getByText('Form Layouts').click();
-});
 
+test('Locator syntax rules', async ({page}) => {
+    // by tag name
+    page.locator('input').first().click();
 
-test('navigate to datepicker page', async ({page}) => {
-    await page.getByText('Datepicker').click();
+    //by id
+    await page.locator('#inputEmail1')
+    
+    //by class name
+    page.locator('.shape-rectangle');
+    
+    //by attribute
+    page.locator('[placeholder="Email"]');
+
+    //by Class value (full)
+    page.locator('class="input-full-width size-medium status-basic shape-rectangle nb-transition"');
+
+    // combine different locators
+    page.locator('input#inputEmail1.shape-rectangle');
+
+    // by xpath
+    page.locator('//input[@id="inputEmail1"]'); // not recommended
+
+    // by partial text match
+    page.locator(':text("Using")');
+
+    // by exact text match
+    page.locator('text="Using the Grid"');
 });
